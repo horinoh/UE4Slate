ebefd244602d70f80e5bd6ae75763e2f863950fc
horino <development@horino.sakura.ne.jp>
2016-01-30 18:23:54 +0900

    C++コード追加

---
+++ Source/UE4Slate/Private/UE4SlateGameViewportClient.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4Slate.h"
+#include "UE4SlateGameViewportClient.h"
+
+
+
+
---
+++ Source/UE4Slate/Private/UE4SlateGameViewportClient.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/GameViewportClient.h"
+#include "UE4SlateGameViewportClient.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UUE4SlateGameViewportClient : public UGameViewportClient
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
